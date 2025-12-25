1deb72c9ea33bb6302717f2bef271693516b58b6
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-25 23:01:07 +0530

    feat: Removed Comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,25 +4,21 @@ import { adminGuardTsGuard } from './core/guards/admin-guard.ts-guard';
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
 
-  // Auth Module: Login, Register, Change Password
   {
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
 
-  // Flight Module: Search and Booking
   {
     path: 'flights',
     loadChildren: () => import('./modules/flight/flight.routes').then(m => m.FLIGHT_ROUTES)
   },
 
-  // User Module: Profile and History
   {
     path: 'profile',
     loadChildren: () => import('./modules/user/user.routes').then(m => m.USER_ROUTES)
   },
 
-  // Admin Module: Guarded by AdminGuard
   {
     path: 'admin',
     loadChildren: () => import('./modules/admin/admin.routes').then(m => m.ADMIN_ROUTES),
